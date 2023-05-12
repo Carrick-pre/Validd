30e098fce7453806e0fd8292828ae9fbadc27649
false <vlad@live.no>
2023-05-12 12:02:31 +0100

    pom

---
+++ src/main/java/com/ericsson/validinvalids/ValidInvalidsApplication.java
@@ -0,0 +1,13 @@
+package com.ericsson.validinvalids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidInvalidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidInvalidsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericsson/validinvalids/ValidInvalidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsson.validinvalids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidInvalidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
