77eaacd427844f5600e479f81df9b7e81296a86c
false <vlad@live.no>
2023-05-12 13:49:58 +0100

    dfdf

---
+++ consumer/src/main/java/Greeting.java
@@ -0,0 +1,12 @@
+
+public class Greeting {
+    private String greeting;
+
+    public Greeting(String message) {
+        this.greeting = message;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
